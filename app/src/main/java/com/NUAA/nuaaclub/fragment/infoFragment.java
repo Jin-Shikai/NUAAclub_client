234c9483e1bb74c9ed8c9329a9a17291201473c1
jinshikai <1784257883@qq.com>
2020-10-04 23:52:20 +0800

    login with check code

--- app/src/main/java/com/NUAA/nuaaclub/fragment/infoFragment.java
+++ app/src/main/java/com/NUAA/nuaaclub/fragment/infoFragment.java
@@ -24,22 +24,32 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.NUAA.nuaaclub.EssayActivity;
 import com.NUAA.nuaaclub.MainActivity;
 import com.NUAA.nuaaclub.R;
 import com.NUAA.nuaaclub.base.BaseFragment;
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.UnsupportedEncodingException;
 import java.security.PrivateKey;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 public class infoFragment extends BaseFragment{
     private RadioGroup mRg_main;
@@ -52,6 +62,8 @@ public class infoFragment extends BaseFragment{
     private String ID;
     private String Password;
     private String Status;
+    private Button getCodeBtn;
+    private String code;
 //
 //    @Nullable
 //    @Override
@@ -99,59 +111,85 @@ public class infoFragment extends BaseFragment{
                     Password = s.toString();
                 }
             });
+            getCodeBtn = (Button) view.findViewById(R.id.getcode);
+            getCodeBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+                    Random random = new Random();
+                    code = String.valueOf(random.nextInt(10000) % (10000 - 1000 + 1) + 1000);
+                    if (ID.length() != 11)
+                        Toast.makeText(mContext, "手机号格式不正确", Toast.LENGTH_SHORT).show();
+                    else {
+                        String url = "https://tianqiapi.com/api/sms?appid=55334614&appsecret=3dLpfLYk&code=" + code + "&mobile=" + ID;
+                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<org.json.JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject jsonObject) {
+                                try {
+                                    if (jsonObject.get("errcode").equals(0))
+                                        Toast.makeText(mContext, "验证码发送成功", Toast.LENGTH_SHORT).show();
+                                    else
+                                        Toast.makeText(mContext, "验证码发送失败", Toast.LENGTH_SHORT).show();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                                Toast.makeText(mContext, "网络似乎不通了", Toast.LENGTH_SHORT).show();
+                            }
+                        }) {
+                            @Override
+                            protected Response<org.json.JSONObject> parseNetworkResponse(NetworkResponse response) {
+                                response.headers.put("HTTP.CONTENT_TYPE", "utf-8");
+                                JSONObject jsonObject;
+                                try {
+                                    jsonObject = new JSONObject(new String(response.data, "UTF-8"));
+                                    return Response.success(jsonObject, HttpHeaderParser.parseCacheHeaders(response));
+                                } catch (UnsupportedEncodingException e) {
+                                    e.printStackTrace();
+                                    return Response.error(new ParseError(e));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                    return Response.error(new ParseError(e));
+                                }
+                            }
+                        };
+                        //5. 将请求添加入请求队列
+                        requestQueue.add(jsonObjectRequest);
+                    }
+                }
+            });
 
             //登录按钮监听
             mBtnLogin = (Button) view.findViewById(R.id.loginbtn);
             mBtnLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    editor.remove("ID");
-                    editor.putString("ID", ID);
-                    //1. 创建请求队列
-                    RequestQueue requestQueue = Volley.newRequestQueue(mContext);
-                    //2. 创建post请求
-                    String url = "http://"+getResources().getString(R.string.address)+":8080/LoginDemo/LoginServlet";
-                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String s) {
-                            Log.i("s", s);
-                            if (s.length() > 10) {
-                                Toast.makeText(mContext, "登录成功, 欢迎您", Toast.LENGTH_SHORT).show();
-                                //步骤3：将获取过来的值放入文件
-                                editor.remove("token");
-                                editor.putString("token", s);
-                                //步骤4：提交
-                                editor.commit();
-                                MainActivity mainActivity = (MainActivity)getActivity();
-                                BaseFragment to = mainActivity.getFrament(3);
-                                //替换
-                                mainActivity.switchFragment(infoFragment.this,to);
-                            } else
-                                Toast.makeText(mContext, "登录失败", Toast.LENGTH_SHORT).show();
-                        }
-                    }, new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError volleyError) {
-                            Toast.makeText(mContext, "网络似乎不通了", Toast.LENGTH_SHORT).show();
-                        }
-                    }) {
-                        @Override
-                        protected Map<String, String> getParams() throws AuthFailureError {
-                            Map<String, String> map = new HashMap<String, String>();
-                            Status="2";
-                            //map.put("value1","param1");
-                            map.put("ID", ID);
-                            map.put("password", Password);
-                            map.put("status",Status);
-                            return map;
-                        }
-                    };
-                    //3. 将请求添加入请求队列
-                    requestQueue.add(stringRequest);
+                    if(code.length()!=4)
+                        Toast.makeText(mContext, "验证码格式不正确", Toast.LENGTH_SHORT).show();
+                    else if (code.equals(Password)) {
+                        editor.remove("ID");
+                        editor.putString("ID", ID);
+                        //生成token
+                        String token = UUID.randomUUID() + "";
+                        editor.remove("token");
+                        editor.putString("token", token);
+                        editor.commit();
+                        MainActivity mainActivity = (MainActivity) getActivity();
+                        BaseFragment to = mainActivity.getFrament(3);
+                        //替换
+                        mainActivity.switchFragment(infoFragment.this,to);
+                    }
+                    else{
+                        Toast.makeText(mContext, "登录失败", Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
         return view;
     }
+
     @Override
     protected void initData() {
         super.initData();
